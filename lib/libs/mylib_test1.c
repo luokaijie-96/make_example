e310210d8dbab7593d1adeb9dd2fc270ccf795da
Leo <1697065378@qq.com>
2023-10-25 23:03:48 +0800

    lib 目录使用模式规则.

---
+++ lib/libs/mylib_test1.c
@@ -0,0 +1,7 @@
+#include "mylib.h"
+#include "stdio.h"
+
+void mylib_test1()
+{
+    printf("This is %s()\r\n", __func__);
+}
---
+++ lib/libs/mylib_test2.c
@@ -0,0 +1,7 @@
+#include "mylib.h"
+#include "stdio.h"
+
+void mylib_test2()
+{
+    printf("This is %s()\r\n", __func__);
+}
